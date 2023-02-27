5b6861651009e1ab4db17d303fa66c07a4fa7593
Redwood4413 <redwoodredwoodowski@gmail.com>
2023-02-27 19:19:01 +0100

    Initial version

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import ResourceCard from './components/ResourceCard.vue';
+
+export default {
+  components: {
+    ResourceCard,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <ul v-for="resource in storedResources" :key="resource.id">
+    <ResourceCard :id="resource.id" :title="resource.title" :description="resource.description" :link="resource.link" />
+  </ul>
+</template>
---
+++ src/components/ResourceCard.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <h2>{{ title }}</h2>
+    <span> {{ description }}</span>
+    <a :href="link">Open</a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    h2{
+      margin: 0;
+    }
+  .card {
+    box-shadow: 8px 8px 24px 0px rgba(0, 0, 0, 1);
+    border-radius: 20px;
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap:10px;
+    a {
+      background: rgb(14, 14, 14);
+      width:100px;
+      color:white;
+      padding: 5px;
+      border-radius: 20px;
+      &:hover {
+        background: rgb(60, 57, 57);
+      }
+    }
+  }
+</style>
